faa4ae6af15bff8bfe93a5cfa189468f6a91aa32
sanoobfreelance <sanoobfreelance@gmail.com>
2026-05-22 14:10:04 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/ci-cd-test-app/",
 
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
 });
